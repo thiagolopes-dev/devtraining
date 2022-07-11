cdc7856895a7d895f008a182d58a492b72b80f51
Thiago Lopes <thiagoolopezz@gmail.com>
2022-07-11 14:00:44 -0300

    Aula 11
    Injetando o Service no Controller para usar os métodos

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -5,35 +5,28 @@ import { CourseService } from './courses.service';
 @Controller('courses')
 export class CoursesController {
 
-    constructor(private readonly coourserService: CourseService) {}
-
-
-    // @Get()
-    // findAll(){
-    //     return 'Listagem de cursos';
-    // }
+    constructor(private readonly coursesService: CourseService) {}
 
     @Get()
-    findAll(@Res() response) {
-        return response.status(200).send('Listagem de Cursos');
+    findAll() {
+        return this.coursesService.findAll();
     }
 
     @Get(':id')
     getByID(@Param('id') id: string) {
-        return `Curso ADS ${id}`;
+        return  this.coursesService.findOne(id);
     }
     @Post()
-    @HttpCode(HttpStatus.NO_CONTENT)
     create(@Body() body) {
-        return body;
+        return this.coursesService.create(body);
     }
     @Patch(':id')
     update(@Param('id') id: string, @Body() body) {
-        return `Atualização do Curso ${id}`;
+        return this.coursesService.update(id, body);
     }
     @Delete(':id')
     remove(@Param('id') id : string, @Body() body){
-        return `Curso deletado${id}`;
+        return this.coursesService.remove(id);
     }
 
 }
