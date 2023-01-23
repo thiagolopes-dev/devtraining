2ef2d8fddf1efd4705b1d4afc321a2af2ddb3ab0
Thiago Lopes <thiagoolopezz@gmail.com>
2023-01-23 20:39:54 -0300

    20. Criando course typeorm database

--- src/courses/courses.module.ts
+++ src/courses/courses.module.ts
@@ -1,8 +1,11 @@
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
 import { CoursesController } from './courses.controller';
 import { CourseService } from './courses.service';
+import { Course } from './entities/course.entity';
 
 @Module({
+    imports:[TypeOrmModule.forFeature([Course])],
 
     controllers: [
         CoursesController
--- src/courses/entities/course.entity.ts
+++ src/courses/entities/course.entity.ts
@@ -1,6 +1,20 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity('courses')
 export class Course {
+
+    @PrimaryGeneratedColumn()
     id: number;
+
+    @Column()
     name: string;
+
+    @Column()
     description: string;
+
+    @Column({
+        type: 'json',
+        nullable: true
+    })
     tags: string[];
 }
